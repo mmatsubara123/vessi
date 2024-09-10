1e59bf541aac3b79d2605626971f148d353ba0c7
mmatsubara <michiko.a9@gmail.com>
2024-09-10 14:03:28 +0900

    vessiサイト（作業中）

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    var menuIcon = document.getElementById("menu-icon");
+    menu.classList.toggle("open");
+    menuIcon.classList.toggle("open");
+}
